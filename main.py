f82a7d824aab70b006d38fe9ae0f24dc0f072d14
Johnathan Maytliss <Johnnie172@Noas-MacBook-Pro.local>
2020-07-29 17:47:48 +0300

    adding exception

--- main.py
+++ main.py
@@ -15,13 +15,16 @@ def index():
 @app.route('/weather', methods=['GET'])
 def get_weather():
     #todo get exepctions and error
-    city = request.args.get('query', 'fetch:ip')
-    api_response = requests_utilities.get_weather_by_location_json(city)
-    weather_dict = requests_utilities.get_info_from_json(api_response)
-    weather_dict['current_visibility'] = utilities.visibility_scale_to_desc(weather_dict['current_visibility'])
-    weather_dict['current_desc'] = utilities.change_desc_to_alphabet_only(weather_dict['current_desc'])
-    weather_str = utilities.make_string_for_weather(weather_dict)
-
+    try:
+        city = request.args.get('query', 'fetch:ip')
+        api_response = requests_utilities.get_weather_by_location_json(city)
+        weather_dict = requests_utilities.get_info_from_json(api_response)
+        weather_dict['current_visibility'] = utilities.visibility_scale_to_desc(weather_dict['current_visibility'])
+        weather_dict['current_desc'] = utilities.change_desc_to_alphabet_only(weather_dict['current_desc'])
+        weather_str = utilities.make_string_for_weather(weather_dict)
+    except:
+        raise Exception('An error occurred!')
+    
     return render_template('weather.html', weather_str=weather_str, api_response=api_response, weather_dict=weather_dict)
 
 @app.route('/choose_city', methods=['GET'])
