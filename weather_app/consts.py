837f9ef9b27006de43fca10b867a0691ba7675e1
Johnathan Maytliss <Johnnie172@MacBook-Pro.local>
2020-07-31 14:07:20 +0300

    fixing key bug

--- weather_app/consts.py
+++ weather_app/consts.py
@@ -3,7 +3,7 @@ import os
 WEB_FOR_CITY = 'https://ipinfo.io/'
 WEB_FOR_WEATHER = 'http://api.weatherstack.com/current'
 
-ACCESS_KEY = os.environ.get['WEATHER_KEY']
+ACCESS_KEY = os.environ.get('WEATHER_KEY')
 
 INTERNET_CONNECTION_OK_MESSEGE = 'Internet connection ok!'
 INTERNET_CONNECTION_FAIL_MESSEGE = 'Connection to internet failed!'
