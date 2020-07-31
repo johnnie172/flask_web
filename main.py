4c8defd8915f541a34f15b310d1fc8fdbd787fe8
Johnathan Maytliss <Johnnie172@MacBook-Pro.local>
2020-07-31 13:00:01 +0300

    adding ip log

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ def get_weather():
     try:
         client_ip = request.headers.get('X-Forwarded-For', 'fetch:ip')
         city = request.args.get('query', client_ip)
+        app.logger.info('client ip is:', client_ip)
         api_response = requests_utilities.get_weather_by_location_json(city)
         weather_dict = requests_utilities.get_info_from_json(api_response)
         weather_dict['current_visibility'] = utilities.visibility_scale_to_desc(weather_dict['current_visibility'])
