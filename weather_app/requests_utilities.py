390178452f96820eecf0cc0b9241e7be2faf85ea
Johnathan Maytliss <Johnnie172@MacBook-Pro.local>
2020-10-12 15:50:11 +0300

    Fixing .format

--- weather_app/requests_utilities.py
+++ weather_app/requests_utilities.py
@@ -36,9 +36,9 @@ def get_weather_by_location_json(city):
         'query': city
     }
     api_result = requests.get(consts.WEB_FOR_WEATHER, params)
-    logger.info('The api request is:'.format(api_result))
+    logger.info('The api request is: {}'.format(api_result))
     api_response = api_result.json()
-    logger.info('The api response is:'.format(api_response))
+    logger.info('The api response is: {}'.format(api_response))
 
     if 'request' not in api_response:
         logger.error('No response from api')
